0e7773bac75d509c5ee3e492dba854c790c93c35
mahmoud <mahmoudghareeb11111@gmail.com>
2022-10-12 12:52:26 +0200

    adding rtl to course page

--- application/views/frontend/default/index.php
+++ application/views/frontend/default/index.php
@@ -68,7 +68,7 @@
 		}
 	</style>
 </head>
-<body class="gray-bg">
+<body class="gray-bg" dir="rtl">
 	<?php
 	if(get_settings('course_accessibility') == 'publicly' || $this->session->userdata('user_id') > 0){
 		$course_accessibility = 1;
