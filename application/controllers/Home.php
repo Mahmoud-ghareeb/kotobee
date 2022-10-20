9319ae8e7c1564c42d4d2960430130d32cca439d
mahmoud <mahmoudghareeb11111@gmail.com>
2022-10-20 15:31:31 +0200

    logged in user

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -654,8 +654,8 @@ class Home extends CI_Controller
         }
 
         // Check if the lesson contained course is purchased by the user
-        $free = $this->crud_model->get_free_lessons($lesson_id);
-        if(!empty($free)){
+        $free['lesson'] = $this->crud_model->get_free_lessons($lesson_id);
+        if(!empty($free['lesson'])){
         if (isset($page_data['lesson_id']) && $page_data['lesson_id'] > 0 && $course_details['course_type'] == 'general') {
             if ($this->session->userdata('role_id') != 1 && !in_array($user_id, $course_instructor_ids)) {
                 if (!is_purchased($course_id)) {
