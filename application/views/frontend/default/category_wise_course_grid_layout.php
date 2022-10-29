ee5096bd2ccb5baedca5042bc3e6eec6a4ece041
mahmoud <mahmoudghareeb11111@gmail.com>
2022-10-29 14:14:20 +0200

    edit home page

--- application/views/frontend/default/category_wise_course_grid_layout.php
+++ application/views/frontend/default/category_wise_course_grid_layout.php
@@ -87,6 +87,70 @@
                         </div>
                     </div>
                 </a>
+                <div class="webui-popover-content">
+                    <div class="course-popover-content">
+                        <?php if ($course['last_modified'] == "") : ?>
+                            <div class="last-updated fw-500"><?php echo site_phrase('last_updated') . ' ' . date('D, d-M-Y', $course['date_added']); ?></div>
+                        <?php else : ?>
+                            <div class="last-updated"><?php echo site_phrase('last_updated') . ' ' . date('D, d-M-Y', $course['last_modified']); ?></div>
+                        <?php endif; ?>
+
+                        <div class="course-title">
+                            <a class="text-decoration-none text-15px" href="<?php echo site_url('home/course/' . rawurlencode(slugify($course['title'])) . '/' . $course['id']); ?>"><?php echo $course['title']; ?></a>
+                        </div>
+                        <div class="course-meta">
+                            <?php if ($course['course_type'] == 'general') : ?>
+                                <span class=""><i class="fa fa-book"></i>
+                                    <?php echo $this->crud_model->get_lessons('course', $course['id'])->num_rows() . ' ' . site_phrase('lessons'); ?>
+                                </span>
+                                <span class=""><i class="far fa-clock"></i>
+                                    <?php echo $course_duration; ?>
+                                </span>
+                            <?php elseif ($course['course_type'] == 'scorm') : ?>
+                                <span class="badge bg-light"><?= site_phrase('scorm_course'); ?></span>
+                            <?php endif; ?>
+                            <span class=""><i class="fas fa-closed-captioning"></i><?php echo ucfirst($course['language']); ?></span>
+                        </div>
+                        <div class="course-subtitle"><?php echo $course['short_description']; ?></div>
+                        <div class="what-will-learn">
+                            <ul>
+                                <?php
+                                $outcomes = json_decode($course['outcomes']);
+                                foreach ($outcomes as $outcome) : ?>
+                                    <li><?php echo $outcome; ?></li>
+                                <?php endforeach; ?>
+                            </ul>
+                        </div>
+                        <div class="popover-btns">
+                            <?php if (is_purchased($course['id'])) : ?>
+                                <div class="purchased">
+                                    <a href="<?php echo site_url('home/my_courses'); ?>"><?php echo site_phrase('already_purchased'); ?></a>
+                                </div>
+                            <?php else : ?>
+                                <?php if ($course['is_free_course'] == 1) :
+                                    // if ($this->session->userdata('user_login') != 1) {
+                                    //     $url = "#";
+                                    // } else {
+                                    //     $url = site_url('home/get_enrolled_to_free_course/' . $course['id']);
+                                    // } ?>
+                                    <!-- <a href="<?php //echo $url; ?>" class="btn green radius-10" onclick="handleEnrolledButton()"><?php //echo site_phrase('get_enrolled'); ?></a> --
+                                <?php else : ?>
+                                    <button type="button" class="btn red d-none add-to-cart-btn <?php if (in_array($course['id'], $cart_items)) echo 'addedToCart'; ?> big-cart-button-<?php echo $course['id']; ?>" id="<?php echo $course['id']; ?>" onclick="handleCartItems(this)">
+                                        <?php
+                                        if (in_array($course['id'], $cart_items))
+                                            echo site_phrase('added_to_cart');
+                                        else
+                                            echo site_phrase('add_to_cart');
+                                        ?>
+                                    </button>
+                                    <button class="btn red add-to-cart-btn" type="button" id="course_<?php echo $course['id']; ?>" onclick="handleBuyNow(this)"><?php echo site_phrase('buy_now'); ?></button>
+                                <?php endif; ?>
+                                <button type="button" class="wishlist-btn <?php if ($this->crud_model->is_added_to_wishlist($course['id'])) echo 'active'; ?>" title="Add to wishlist" onclick="handleWishList(this)" id="<?php echo $course['id']; ?>"><i class="fas fa-heart"></i></button>
+                            <?php endif; ?>
+
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     <?php endforeach; ?>
