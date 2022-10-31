1cc8edd4fe53d6a0413e173d03818a0428cf5fe3
mahmoud <mahmoudghareeb11111@gmail.com>
2022-10-31 17:12:01 +0200

    edit kiosk

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1462,7 +1462,7 @@ class Home extends CI_Controller
         $status = $paramList["success"];
         $payment_request_mobile = 'true';
 
-        echo json_encode('hi');
+        echo json_encode($_POST);
         
         // if (true) { //APPROVED
             
