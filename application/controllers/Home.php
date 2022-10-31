cff269d089a728f4a27cc677b3292e7a38b7fb5f
mahmoud <mahmoudghareeb11111@gmail.com>
2022-10-31 17:34:40 +0200

    edit kiosk

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1457,37 +1457,14 @@ class Home extends CI_Controller
     }
     
     public function aman_payment()
-    {
-        // $paramList = $_POST;
-        // $status = $paramList["success"];
-        // $payment_request_mobile = 'true';
-
-        // $input = file_get_contents('php://input');
-        // $data = json_decode($input);
-
-        if(strcasecmp($_SERVER['REQUEST_METHOD'], 'POST') != 0){
-            throw new Exception('Request method must be POST!');
-        }
-        
-        //Make sure that the content type of the POST request has been set to application/json
-        $contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
-        if(strcasecmp($contentType, 'application/json') != 0){
-            throw new Exception('Content type must be: application/json');
-        }
-        
+    {        
         //Receive the RAW post data.
         $content = trim(file_get_contents("php://input"));
-        
         //Attempt to decode the incoming RAW post data from JSON.
         $decoded = json_decode($content, true);
         
-        echo $content;
+        echo $decoded->obj->id;
 
-
-        //If json_decode failed, the JSON is invalid.
-        if(!is_array($decoded)){
-            throw new Exception('Received content contained invalid JSON!');
-        }
         
         //Process the JSON.
         
