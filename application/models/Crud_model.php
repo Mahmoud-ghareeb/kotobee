d7983f65415f3ae1828cbf1f002bf13e45eeffa0
mahmoud <mahmoudghareeb11111@gmail.com>
2022-11-06 22:42:19 +0200

    edits

--- application/models/Crud_model.php
+++ application/models/Crud_model.php
@@ -1861,7 +1861,7 @@ class Crud_model extends CI_Model
             $this->db->where('course_type', 'general');
         }
         $this->db->order_by("id", "desc");
-        // $this->db->limit('10');
+        $this->db->limit('20');
         $this->db->where('status', 'active');
         return $this->db->get('course')->result_array();
     }
