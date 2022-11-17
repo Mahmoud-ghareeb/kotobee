313a5da40bee29630759f929a9d636f175487bc1
mahmoud <mahmoudghareeb11111@gmail.com>
2022-11-17 21:53:36 +0200

    multi

--- application/views/frontend/default/category_wise_course_grid_layout.php
+++
@@ -1,8 +0,0 @@
-<div class="row">
-    <?php foreach ($courses as $course) :
-        $instructor_details = $this->user_model->get_all_user($course['user_id'])->row_array();
-        $course_duration = $this->crud_model->get_total_duration_of_lesson_by_course_id($course['id']);
-        $lessons = $this->crud_model->get_lessons('course', $course['id']); ?>
-        
-    <?php endforeach; ?>
-</div>
