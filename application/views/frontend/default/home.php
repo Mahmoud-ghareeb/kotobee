955cef0fb57ca021248f5f39517b2b53b9e06355
mahmoud <mahmoudghareeb11111@gmail.com>
2022-11-17 21:47:01 +0200

    multi

--- application/views/frontend/default/home.php
+++ application/views/frontend/default/home.php
@@ -449,7 +449,7 @@
         <div class="container-lg">
             <div class="category-course-list">
                 <?php $courses = $this->crud_model->get_latest_10_course() ?>
-                <?php include 'category_wise_course_grid_layout.php'; ?>
+                <?php include 'cateory_wise_course_grid_layout.php'; ?>
                 <?php if (count($courses) == 0) : ?>
                     <?php echo site_phrase('no_result_found'); ?>
                 <?php endif; ?>
